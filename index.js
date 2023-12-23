9d863a09115f26d717d87511511796bafe0f80a5
Luis Tapari Garcia <tapari17@gmail.com>
2023-12-23 15:39:43 -0300

    segunda-pre-entrega-Luis-Tapari

--- index.js
+++ index.js
@@ -1,81 +1,83 @@
-const precioInstrumental = 50;
-const precioSoundKit = 30;
-const precioProduccion = 100;
-const precioPromocion = 75;
+const precios = {
+    instrumental: 50,
+    soundKit: 30,
+    produccion: 100,
+    promocion: 75,
+};
 
+const productos = [
+    { id: 1, nombre: 'Instrumental', precio: precios.instrumental },
+    { id: 2, nombre: 'SoundKit', precio: precios.soundKit },
+    { id: 3, nombre: 'Producción', precio: precios.produccion },
+    { id: 4, nombre: 'Promoción', precio: precios.promocion },
+];
 
-function calcularCostoTotal() {
+function obtenerNombreUsuario() {
     let nombreUsuario;
-
     while (true) {
-        nombreUsuario = prompt("Hola, ingrese su nombre de usuario");
+        nombreUsuario = prompt("Bienvenido! Por favor, ingrese su nombre de usuario");
         if (nombreUsuario) {
             break;
-    }   else {
+        } else {
             alert("Por favor, ingresa tu nombre para continuar.");
         }
     }
+    return nombreUsuario;
+}
 
-    alert(`Hola ${nombreUsuario}! Bienvenido/a a la tienda de música urbana. Donde contamos con una amplia variedad de instrumentales, kits de sonido, producciones y promociones para vos! pensandolo por unidad, por favor indique en la siguiente interaccion la cantidad de cada uno que desea llevar`);
+function deseaComprar(nombreUsuario) {
+    return confirm(`Hola ${nombreUsuario}, ¿deseas comprar algún producto o servicio?`);
+}
 
-
-    const deseaComprar = confirm("¿Deseas comprar algún producto o servicio?");
-
-    if (!deseaComprar) {
-        alert("¡Gracias por visitarnos! ¡Esperamos verte de nuevo pronto!");
-        return;
-    }
-
+function obtenerCantidadProductos() {
     let cantidadProductos;
-
     while (true) {
         cantidadProductos = parseInt(prompt("¿Cuántos productos o servicios deseas comprar?"), 10);
-
         if (!isNaN(cantidadProductos) && cantidadProductos > 0) {
-                break;
-        }else {
+            break;
+        } else {
             alert("Cantidad no válida. Por favor, ingresa un número positivo.");
         }
     }
-
-
-    let costoTotal = 0;
-
-
-    for (let i = 0; i < cantidadProductos; i++) {
-        let seleccion;
-
-        while (true) {
-
-            seleccion = parseInt(prompt("Selecciona el producto o servicio (1: Instrumental ($50), 2: SoundKit ($30), 3: Producción ($100), 4: Promoción ($75)):"), 10);
-
-
-            if (seleccion >= 1 && seleccion <= 4) {
-                break; // Salir del bucle while si la selección es válida
-            } else {
-                alert("Selección no válida. Por favor, elige un número del 1 al 4.");
-            }
-        }
-
-        switch (seleccion) {
-        case 1:
-        costoTotal += precioInstrumental;
-        break;
-        case 2:
-        costoTotal += precioSoundKit;
-        break;
-        case 3:
-        costoTotal += precioProduccion;
-        break;
-        case 4:
-        costoTotal += precioPromocion;
-        break;
-        }
-    }
-
-
-    alert(`¡Gracias ${nombreUsuario}! El costo total de tus productos y/o servicios seleccionados es: $${costoTotal}. ¡Esperamos que disfrutes tu compra!`);
+    return cantidadProductos;
 }
 
+function seleccionarProducto() {
+    let seleccion;
+    while (true) {
+        seleccion = parseInt(prompt("Selecciona el producto (1: Instrumental, 2: SoundKit, 3: Producción, 4: Promoción):"), 10);
+        const productoSeleccionado = productos.find(producto => producto.id === seleccion);
+        if (productoSeleccionado) {
+            return productoSeleccionado;
+        } else {
+            alert("Selección no válida. Por favor, elige un número del 1 al 4.");
+        }
+    }
+}
 
-calcularCostoTotal();
+function obtenerFechaVenta() {
+    const fecha = new Date();
+    return fecha.toLocaleDateString();
+}
+
+function mostrarMensajeFinal(nombreUsuario, costoTotal) {
+    const fechaVenta = obtenerFechaVenta();
+    alert(`¡Gracias ${nombreUsuario}! El costo total de tus productos y/o servicios seleccionados es: $${costoTotal}. Fecha de la venta: ${fechaVenta}. ¡Esperamos que disfrutes tu compra!`);
+}
+
+const nombreUsuario = obtenerNombreUsuario();
+
+if (deseaComprar(nombreUsuario)) {
+    const cantidadProductos = obtenerCantidadProductos();
+
+    const productosSeleccionados = [];
+    for (let i = 0; i < cantidadProductos; i++) {
+        productosSeleccionados.push(seleccionarProducto());
+    }
+
+    const costoTotal = productosSeleccionados.reduce((total, producto) => total + producto.precio, 0);
+
+    mostrarMensajeFinal(nombreUsuario, costoTotal);
+} else {
+    alert("¡Gracias por visitarnos! ¡Esperamos verte de nuevo pronto!");
+}
